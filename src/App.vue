382f9757f9bc94382ffda5315989f973da50362a
duankai0610 <duankai0610@163.com>
2019-10-09 11:30:56 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <keep-alive>
+        <router-view></router-view>
+    </keep-alive>
+    <TabBar v-if="$route.meta.tabBar"/>
+  </div>
+</template>
+
+<script>
+import TabBar from "common/tabBar"
+export default {
+    name:"App",
+    components:{
+      TabBar,
+      
+    }
+}
+</script>
+<style lang="scss">
+
+</style>
